afbb73e87cdf22c5dda2ce0ff4ef2089297663f1
hitcherland <hitcherland@gmail.com>
2025-05-30 23:09:33 +0100

    Add SCSS compilation via turf

--- src/components/app.rs
+++ src/components/app.rs
@@ -1,11 +1,14 @@
 use leptos::prelude::*;
 
+turf::style_sheet!("src/scss/main.scss");
+
 #[component]
 pub fn App() -> impl IntoView {
-    let (count, set_count) = signal(0);
+    let (count, click_count) = signal(0);
 
     view! {
-        <button on:click=move |_| set_count.set(3)>"Click me: " {count}</button>
+        <style>{STYLE_SHEET}</style>
+        <button on:click=move |_| click_count.update(|n| *n += 1)>"Click me: " {count}</button>
         <p>"Double count: " {move || count.get() * 2}</p>
     }
 }
